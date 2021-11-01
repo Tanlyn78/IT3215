e6e8664a36570fc911b9ec523972eaff5a34808d
Tanlyn78 <88503640+Tanlyn78@users.noreply.github.com>
2021-10-31 19:32:18 -0700

    Update indexbanner.js

--- js/indexbanner.js
+++ js/indexbanner.js
@@ -1,29 +1,32 @@
-//The JavaScript onload event handler 
+//starting rotate function on window load 
 window.onload = rotate;
 
-//declare  variable 
+//declare  variables and initiliaze cycle image
 var cycleImg = 0;
 var bannerImgs = new Array("images/banner1.jpg","images/banner2.jpg","images/banner3.jpg");
 
-
-//The function to rotate the images.
+//starting the "rotate" function
 function rotate() {
     
-//rotating images...
+//cycling through images with if statement
     cycleImg++;
     if (cycleImg == bannerImgs.length) {
         cycleImg = 0;
     }
+    //location of element that will cycle through images from html document - I added an ID
+    //to the html document in this section to point to
     document.getElementById("tannaBanner").src = bannerImgs[cycleImg];
-
+//setting image change time to 3 seconds
     setTimeout("rotate()", 3 * 1000);
 }
+//function for mouseover event for gallery images to enlarge images -the mouseover and mouseout function calls
+//are added to the HTML
 function bigImg(Bimg) {
-    Bimg.style.height="400px";
-    Bimg.style.width="500px";
+    Bimg.style.height="500px";
+    Bimg.style.width="300px";
 }
 
 function normalImg(Nimg) {
-    Nimg.style.height="200px";
-    Nimg.style.width="300px";
+    Nimg.style.height="300px";
+    Nimg.style.width="200px";
 }
